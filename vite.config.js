5bd440eb82dfedb1aea10a5ebd4d3f080095eed3
YADAVSOMURAMYAS <somuuuyadav@gmail.com>
2025-06-21 19:29:59 +0530

    fix: ensure base path is correctly set for Currency Converter

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/CurrencyConverter/',
   plugins: [react(), tailwindcss()],
   
 })
